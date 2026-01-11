4358beae5347d8cd23ae7b988dc7b57515b4cf62
zael99 <zael99@gmail.com>
2026-01-11 10:26:06 -0500

    commit

--- dot_config/hypr/scripts/executable_autostart.sh
+++ dot_config/hypr/scripts/executable_autostart.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland
-
-systemctl --user stop hyprland-session.target xdg-desktop-portal xdg-desktop-portal-hyprland
-systemctl --user start hyprland-session.target
 
 #syncthing &
 steam -silent &
