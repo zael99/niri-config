020a408b668f984e74e17f862a48ae79cdcc5de0
zael99 <bdinelt@gmail.com>
2026-01-16 17:06:34 +0000

    Moving to KDE

--- dot_config/hypr/scripts/executable_autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#syncthing &
-steam -silent &
-heroic &
-#sleep 1
-
-#sleep 1
-#set-random-wallpaper
--- dot_config/hypr/scripts/executable_logout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Close all windows gracefully before exiting
-hyprctl clients -j | jq -r '.[] | .address' | xargs -I {} hyprctl dispatch closewindow address:{}
-sleep 0.5
-hyprctl dispatch exit
